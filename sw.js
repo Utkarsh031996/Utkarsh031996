680e9635bdd07f055da31a68989b3fc5b3b57521
Utkarsh <utkarsh@example.com>
2026-07-14 00:02:06 +0530

    Add PWA support and landing page

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'interest-calculator-v1';
+const ASSETS = [
+  './',
+  './interest_calculator.html',
+  './manifest.json',
+  './icons/icon-192.svg',
+  './icons/icon-512.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => {
+      if (key !== CACHE_NAME) return caches.delete(key);
+    }))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
